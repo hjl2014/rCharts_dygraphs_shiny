b4527640c59a9b36214cd2ac790e8c8031542c93
timelyportfolio <kent.russell@timelyportfolio.com>
2014-08-07 13:26:14 -0500

    make sync = T to highlight this feature

--- server.R
+++ server.R
@@ -12,14 +12,14 @@ shinyServer(function(input, output, session){
   SPY$Momentum20days <- momentum(SPY$SPY.Close, 20)/lag(SPY$SPY.Close, 20)*100
    
   output$mygraph <- renderChart2({
-    dygraph(data=SPY[,c("Date","SPY.Close")], sync=F, crosshair="vertical", legendFollow=TRUE,  height = NULL, width = NULL)
+    dygraph(data=SPY[,c("Date","SPY.Close")], sync=T, crosshair="vertical", legendFollow=TRUE,  height = NULL, width = NULL)
   })
   
   output$mygraph2 <- renderChart2({
-    dygraph(data=SPY[,c("Date","SPY.Close")], sync=F, crosshair="vertical", legendFollow=TRUE, height = 400, width = 1000)
+    dygraph(data=SPY[,c("Date","SPY.Close")], sync=T, crosshair="vertical", legendFollow=TRUE, height = 400, width = 1000)
   })
   
   output$mygraph3 <- renderChart2({
-    dygraph(data=SPY[,c("Date","SPY.Close")], sync=FALSE, crosshair="vertical", legendFollow=TRUE, height = 400, width = 400)
+    dygraph(data=SPY[,c("Date","SPY.Close")], sync=T, crosshair="vertical", legendFollow=TRUE, height = 400, width = 400)
   })
 })
